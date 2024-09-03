8f3633506cbed42c3453f668866d45e353a7d94b
ExCode Env <122183049+royalhomie@users.noreply.github.com>
2024-09-03 19:58:23 +0000

    Update portfolio website with recent changes

--- scripts.js
+++ scripts.js
@@ -15,6 +15,20 @@ const write = new Typed(".Animate", {
     loop: true
 });
 
+// ===== TAWK====
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/66d62abc50c10f7a00a33683/1i6q9e34r';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+
+// === TAWK END===
+
 //=====TO PREVENT TEXT COPYING============
 
 
